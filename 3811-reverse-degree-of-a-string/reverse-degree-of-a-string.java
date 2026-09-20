41bc6fd6076256eff64ddc021091d3559b7be085
Ashhar Ahmad Raza <razaashhar2002@gmail.com>
2026-09-20 21:11:03 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (67.44%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -2,16 +2,13 @@ class Solution {
     public int reverseDegree(String s) {
         
         int ans = 0;
-        int[] val = new int[26];
-        for(int i = 0 ; i < 26 ; i++)
-        {
-            val[i] = 26 - i;
-            
-        }
+    
         for(int i = 0 ; i < s.length() ; i++)
         {
             char ch = s.charAt(i);
-            ans = (val[ch - 'a'] * (i+1)) + ans;
+
+            int val = 26 - (ch - 'a');
+            ans += val * (i+1);
         }
 
         return ans;
